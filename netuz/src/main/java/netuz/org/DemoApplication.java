562f4a3fc094539bc4cbc537803595e2c6596e98
Asadbek <nizomovasadbek@gmail.com>
2022-01-07 15:10:21 +0500

    Student controller

--- netuz/src/main/java/netuz/org/DemoApplication.java
+++ netuz/src/main/java/netuz/org/DemoApplication.java
@@ -3,28 +3,12 @@ package netuz.org;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import netuz.org.student.Student;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }
-
-    @GetMapping
-    public List<Student> guide(){
-        List<Student> st = new ArrayList<>();
-        st.add(new Student(1L, "Asadbek", "nizomovasadbekjava@gmail.com",
-                LocalDate.of(2003, 8,2), 18));
-        return st;
     }
 
 }

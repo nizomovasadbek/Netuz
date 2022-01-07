0cc54b1603a052c31d2136ad4ea0723d50305c52
Asadbek <nizomovasadbek@gmail.com>
2022-01-07 14:47:55 +0500

    commit2

--- netuz/src/main/java/netuz/org/student/Student.java
+++ netuz/src/main/java/netuz/org/student/Student.java
@@ -1,6 +1,10 @@
 package netuz.org.student;
 
+import java.time.LocalDate;
+
 public class Student {
     private long id;
     private String name;
+    private LocalDate dob;
+    private Integer age;
 }

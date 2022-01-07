0d790789f83e5ab213e60fdfe6ba51241102f65b
Nizomov <48754111+nizomovasadbek@users.noreply.github.com>
2022-01-07 14:49:13 +0500

    Update Student.java

--- netuz/src/main/java/netuz/org/student/Student.java
+++ netuz/src/main/java/netuz/org/student/Student.java
@@ -1,10 +1,13 @@
 package netuz.org.student;
 
 import java.time.LocalDate;
+import org.springframework.annotation.web.Autowired
 
 public class Student {
     private long id;
     private String name;
+    @Autowired
+    @Qualifier("rockMusic")
     private LocalDate dob;
     private Integer age;
 }
